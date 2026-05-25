fb31cc63f92eec712fd0cc761b26c7ac419c2bc3
naalab22 <naalab22@pm.me>
2026-05-25 15:48:27 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+
+        let mut count_s: HashMap<u8, i32> = HashMap::new();
+        let mut count_t: HashMap<u8, i32> = HashMap::new();
+
+        for (a, b) in s.bytes().zip(t.bytes()) {
+            *count_s.entry(a).or_insert(0) += 1;
+            *count_t.entry(b).or_insert(0) += 1;
+        }
+        count_s == count_t
+    }
+}
